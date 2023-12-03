3f6ce742119eac6f741da2fc9320a06b862ff790
Fabian Harrison <2023622802@student.uitm.edu.my>
2023-12-03 17:16:41 +0800

    - Up to Slide 58

---
+++ app/src/main/java/com/example/lab3_additionalsqliteimageupload/Food.java
@@ -0,0 +1,24 @@
+package com.example.lab3_additionalsqliteimageupload;
+
+public class Food {
+    private int id;
+    private String name;
+    private String price;
+    private byte[] image;
+
+    public Food(String name, String price, byte[] image, int id){
+        this.name = name;
+        this.price = price;
+        this.image = image;
+        this.id = id;
+    }
+
+    public int getId(){return id;}
+    public void setId(int id){this.id = id;}
+    public String getName(){return name;}
+    public void setName(String price){this.name = name;}
+    public String getPrice(){return price;}
+    public void setPrice(String price){this.price = price;}
+    public byte[] getImage(){return image;}
+    public void setImage(byte[] image){this.image = image;}
+}
